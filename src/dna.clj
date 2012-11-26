ea73fbf1ab6519fc92b0a0c39cdc3c584b36dca9
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2012-11-26 22:31:12 +0200

    solved first problem

---
+++ src/dna.clj
@@ -0,0 +1,11 @@
+(require 'clojure.string)
+
+; read lines, save first line
+(def dna ((clojure.string/split-lines (slurp "dna.txt")) 0))
+
+(defn count-strand [strand dna]
+  (count (filter #(= % strand) dna)))
+
+(def counts (map #(count-strand % dna) [\A \C \G \T]))
+
+(println (clojure.string/join " " counts))
